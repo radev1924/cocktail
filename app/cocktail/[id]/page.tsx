81e329cebbd028e994a655804dc72286c7d1bac9
radev1924 <radev1924@abv.bg>
2024-09-14 23:49:24 +0300

    final adjustments and rebuild for deployment

--- app/cocktail/[id]/page.tsx
+++ app/cocktail/[id]/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'next/navigation';
-import Image from 'next/image';
 
 interface Cocktail {
   idDrink: string;
