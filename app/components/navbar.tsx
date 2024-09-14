4c5d4cf3433485130764a1d4d5b1d0ce8b62290b
radev1924 <radev1924@abv.bg>
2024-09-14 22:46:05 +0300

    Implemented the navbar into the layout

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex items-center justify-between">
         <Link href="/" className="flex items-center text-white text-2xl font-bold">
-          <span>Cocktail</span>
+          <span>CocktailDB</span>
         </Link>
       </div>
     </nav>
